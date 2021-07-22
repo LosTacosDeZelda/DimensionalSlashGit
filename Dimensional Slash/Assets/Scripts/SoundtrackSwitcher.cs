221ea87d5f2a2bcf834d01e60b6153fb8fca4e3c
Ludovick Lubérisse <Luberisselud@gmail.com>
2021-07-22 13:34:06 -0400

    Soundtrack Switch
    Update du soundtrack switcher + FMOD

--- Dimensional Slash/Assets/Scripts/SoundtrackSwitcher.cs
+++ Dimensional Slash/Assets/Scripts/SoundtrackSwitcher.cs
@@ -5,16 +5,50 @@ using UnityEngine;
 //Ludo : Ce script est pour toi, va falloir que tu set tes paramètres de FMOD ici.
 public class SoundtrackSwitcher : MonoBehaviour
 {
+    //Les events du Jukebox et de l'Ambiance sonore
+    //public FMODUnity.StudioEventEmitter SoundEvent;
+    [FMODUnity.EventRef]
+    public string SoundEvent = "";
+
+    [FMODUnity.EventRef]
+    public List<string> Events;
+
+    FMOD.Studio.EventInstance EventInstance;
     private void OnEnable()
     {
         GameManager.OnDimensionChange += SwitchSoundtrack;
+        //créer une instance de l'event proposé et le faire jouer manuellement
+        EventInstance = FMODUnity.RuntimeManager.CreateInstance(SoundEvent);
+        EventInstance.start();
     }
 
     //La fonction SwitchSoundtrack est appelée quand le joueur change de dimension.
     //À toi de set les paramètres nécessaires de FMOD !
     void SwitchSoundtrack(GameManager._Dimensions dimension)
     {
-
+        switch (dimension)
+        {
+            case GameManager._Dimensions.Day:
+                SoundEvent = "event:/Ambiance/Wind";
+                StartCoroutine(FadeOut());
+                break;
+            case GameManager._Dimensions.Night:
+                SoundEvent = "event:/Ambiance/Fire";
+                StartCoroutine(FadeOut());
+                break; 
+            default:
+                break;
+        }
+    }
+    // On attend que le fade out termine avant de jouer le nouveau son
+    IEnumerator FadeOut()
+    {
+        GetComponent<FMODUnity.StudioEventEmitter>().Event = SoundEvent;
+        EventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        yield return new WaitForSeconds(5f);
+        EventInstance.release();
+        // EventInstance.clearHandle();
+        EventInstance.start();
     }
 
     private void OnDisable()
